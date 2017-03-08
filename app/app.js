163fe3bc55d3848707e03e6fff356b1010442357
Asheesh Chaudhary <Asheesh Chaudhary>
2017-03-08 23:37:13 +0530

    Extra code fix

--- app/app.js
+++ app/app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var myApp = angular.module("myApp", ["angularUtils.directives.dirPagination"]);
 
 myApp.controller("MainController", ['$scope', '$http', function($scope, $http){
@@ -33,15 +32,3 @@ myApp.controller("MainController", ['$scope', '$http', function($scope, $http){
     });
 
 }]);
-=======
-var myApp = angular.module("myApp", []);
-
-myApp.controller("MainController", ['$scope', '$http', function($scope, $http){
-
-  $http.get('data/data.json').then(function(response){
-    $scope.bookings = response.data;
-    $scope.book = response.data.appointments.appointment.city.name;
-  });
-
-}]);
->>>>>>> 457ef48dcde0577c8df7eaf0ca60e913759f09b1
