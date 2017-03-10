94ad5efb324586affad4b455be42b5bcfcc5416b
asheesh.chaudhary <asheesh.chaudhary@accenture.com>
2017-03-10 17:47:59 +0530

    gulp final

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,35 @@
 var gulp = require('gulp'),
-    uglify = require('gulp-uglify');
+    uglify = require('gulp-uglify'),
+    browserSync = require('browser-sync').create();
 
-gulp.task('default', function(){
+gulp.task('minify', function(){
   gulp.src('app/*.js')
-  .pipe(uglify())
-  .pipe(gulp.dest('minjs'))
-  console.log('Minification is finished.');
+      .pipe(uglify())
+      .pipe(gulp.dest('app'))
+      .pipe(browserSync.reload({
+        stream: true
+      }));
+});
+
+gulp.task('build', function(){
+  gulp.start(['minify']);
+});
+
+gulp.task('browser-sync', function(){
+  browserSync.init(null, {
+    open: false,
+    server: {
+      baseDir: './'
+    }
+  });
+});
+
+gulp.task('start', function(){
+  devMode = true;
+  gulp.start(['build', 'browser-sync']);
+  gulp.watch(['index.html'], browserSync.reload);
+  gulp.watch(['app/*.js'], browserSync.reload);
+  gulp.watch(['data/*.json'], browserSync.reload);
+  gulp.watch(['Content/css/*.css'], browserSync.reload);
+  gulp.watch(['app/*.js'], ['minify'], browserSync.reload);
 });
